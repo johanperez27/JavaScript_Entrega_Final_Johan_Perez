e80a7d866297a0bfc19584a3817b8634a44ef523
johanperez27 <90525356+johanperez27@users.noreply.github.com>
2022-08-29 20:04:15 -0500

    Add files via upload

---
+++ JavaScript_Entrega_Final_Johan_Perez/pages/pages.js
@@ -0,0 +1,12 @@
+
+let boton = document.getElementById("btnPrincipal");
+boton.addEventListener('click', () => {
+
+
+  Swal.fire({
+    title: "FELICITACIONES ",
+    text: "Gracias Por Preferirnos!!!",
+    icon: "success",
+    confirmButtonText: "ACEPTAR",
+  });
+})
